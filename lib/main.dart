3cf3c68f7f0baf9ab2a14d56e08196f4359f53f2
Koba <koba@dummy.com>
2022-02-19 16:16:49 +0900

    CustomPaintで頑張った

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ifs_gui/IFS.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
@@ -27,31 +25,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  IFS _ifs = IFS.shida();
+  final List<Vector> _points = [];
+
+  @override
+  void initState() {
+    Timer.periodic(
+      const Duration(milliseconds: 10),
+      _onTimer,
+    );
+    super.initState();
+  }
+
+  void _onTimer(Timer timer) {
+    const int steps = 10;
+    setState(() => _points.addAll(_ifs.iterate(steps)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
+      body: CustomPaint(
+        size: Size.infinite,
+        painter: MyPainter(_points),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      //floatingActionButton: const FloatingActionButton(
+      //  onPressed: null,
+      //  tooltip: 'Increment',
+      //  child: Icon(Icons.add),
+      //),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final List<Vector> _points;
+
+  const MyPainter(this._points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+    paint.color = Colors.black;
+
+    for (var point in _points) {
+      const int scale = 10;
+      final double x = point.x * scale + size.width / 2;
+      final double y = size.height - 1 - point.y * scale - size.height / 2;
+      canvas.drawRect(Rect.fromLTWH(x, y, 1, 1), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
