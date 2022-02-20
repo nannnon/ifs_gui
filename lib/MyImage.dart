7639fb2289091dd3fc285d5507c2ff41db48420a
Koba <koba@dummy.com>
2022-02-20 15:06:54 +0900

    MyImageクラスを実装

---
+++ lib/MyImage.dart
@@ -0,0 +1,44 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class MyImage {
+  final int _width;
+  final int _height;
+  final Uint8List _pixels;
+
+  MyImage(this._width, this._height)
+      : _pixels = Uint8List(_width * _height * 4) {
+    fill(Colors.black);
+  }
+
+  void fill(Color color) {
+    for (int x = 0; x < _width; ++x) {
+      for (int y = 0; y < _height; ++y) {
+        setPixel(x, y, color);
+      }
+    }
+  }
+
+  void setPixel(int x, int y, Color color) {
+    if (x < 0 || x >= _width || y < 0 || y >= _height) {
+      return;
+    }
+
+    int index = (y * _width + x) * 4;
+    _pixels[index + 0] = color.red;
+    _pixels[index + 1] = color.green;
+    _pixels[index + 2] = color.blue;
+    _pixels[index + 3] = color.alpha;
+  }
+
+  Future<ui.Image> convertToUIImage() async {
+    Completer<ui.Image> c = Completer();
+    ui.decodeImageFromPixels(_pixels, _width, _height, ui.PixelFormat.rgba8888,
+        (ui.Image result) {
+      c.complete(result);
+    });
+    return c.future;
+  }
+}
---
+++ test/MyImage_test.dart
@@ -0,0 +1,20 @@
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ifs_gui/MyImage.dart';
+
+void main() {
+  test('MyImage test', () async {
+    const int w = 124;
+    const int h = 97;
+    MyImage img = MyImage(w, h);
+
+    img.fill(Colors.black);
+    img.setPixel(10, 12, Colors.red);
+    await img.convertToUIImage();
+    ui.Image uiimg = await img.convertToUIImage();
+
+    expect(uiimg.width, w);
+    expect(uiimg.height, h);
+  });
+}
