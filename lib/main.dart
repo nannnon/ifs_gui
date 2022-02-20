b4d84b9461a8073642131862be3ba194d7595e1c
Koba <koba@dummy.com>
2022-02-20 18:34:47 +0900

    切り替えられるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,12 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+enum IFSType { fern, ammonite, leaf }
+
 class _MyHomePageState extends State<MyHomePage> {
-  final IFS _ifs = IFS.shida();
+  IFS _ifs = IFS.fern();
+  double _scale = 50;
+  int _steps = 50;
   final MyImage _image = MyImage(512, 1024, Colors.black);
   ui.Image? _imgBuffer;
 
@@ -42,14 +46,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _onTimer(Timer timer) async {
-    const int steps = 50;
-    final points = _ifs.iterate(steps);
+    final points = _ifs.iterate(_steps);
 
     for (var point in points) {
-      const double scale = 50;
-      final int x = (point.x * scale).round() + _image.width ~/ 2;
+      final int x = (point.x * _scale).round() + _image.width ~/ 2;
       final int y =
-          _image.height - 1 - (point.y * scale).round() - _image.height ~/ 5;
+          _image.height - 1 - (point.y * _scale).round() - _image.height ~/ 2;
 
       _image.setPixel(x, y, Colors.white);
     }
@@ -65,6 +67,40 @@ class _MyHomePageState extends State<MyHomePage> {
       body: InteractiveViewer(
         child: RawImage(image: _imgBuffer),
         constrained: false,
+      ),
+      floatingActionButton: DropdownButton(
+        value: IFSType.fern,
+        items: const [
+          DropdownMenuItem(child: Text('シダ'), value: IFSType.fern),
+          DropdownMenuItem(child: Text('アンモナイト'), value: IFSType.ammonite),
+          DropdownMenuItem(child: Text('葉飾り'), value: IFSType.leaf),
+        ],
+        onChanged: (IFSType? value) {
+          setState(() {
+            _image.fill(Colors.black);
+            switch (value) {
+              case IFSType.fern:
+                _ifs = IFS.fern();
+                _scale = 50;
+                _steps = 50;
+                break;
+              case IFSType.ammonite:
+                _ifs = IFS.ammonite();
+                _scale = 300;
+                _steps = 200;
+                break;
+              case IFSType.leaf:
+                _ifs = IFS.leaf();
+                _scale = 300;
+                _steps = 200;
+                break;
+              case null:
+                break;
+            }
+          });
+        },
+        icon: const Icon(Icons.update),
+        iconSize: 48,
       ),
     );
   }
