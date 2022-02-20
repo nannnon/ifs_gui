d2f2f8d40aa8f41cd9c7b4fb40ecd3ef5327501f
Koba <koba@dummy.com>
2022-02-20 16:04:31 +0900

    ひとまず出来た

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:ifs_gui/IFS.dart';
-import 'dart:async';
+import 'package:ifs_gui/MyImage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,59 +27,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  IFS _ifs = IFS.shida();
-  final List<Vector> _points = [];
+  final IFS _ifs = IFS.shida();
+  final MyImage _image = MyImage(512, 1024, Colors.black);
+  ui.Image? _imgBuffer;
 
   @override
   void initState() {
     Timer.periodic(
-      const Duration(milliseconds: 10),
+      const Duration(milliseconds: 100),
       _onTimer,
     );
+
     super.initState();
   }
 
-  void _onTimer(Timer timer) {
-    const int steps = 10;
-    setState(() => _points.addAll(_ifs.iterate(steps)));
+  void _onTimer(Timer timer) async {
+    const int steps = 50;
+    final points = _ifs.iterate(steps);
+
+    for (var point in points) {
+      const double scale = 50;
+      final int x = (point.x * scale).round() + _image.width ~/ 2;
+      final int y =
+          _image.height - 1 - (point.y * scale).round() - _image.height ~/ 5;
+
+      _image.setPixel(x, y, Colors.white);
+    }
+
+    _imgBuffer = await _image.convertToUIImage();
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomPaint(
-        size: Size.infinite,
-        painter: MyPainter(_points),
-      ),
-      //floatingActionButton: const FloatingActionButton(
-      //  onPressed: null,
-      //  tooltip: 'Increment',
-      //  child: Icon(Icons.add),
-      //),
+      body: RawImage(image: _imgBuffer),
     );
-  }
-}
-
-class MyPainter extends CustomPainter {
-  final List<Vector> _points;
-
-  const MyPainter(this._points);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint();
-    paint.color = Colors.black;
-
-    for (var point in _points) {
-      const int scale = 10;
-      final double x = point.x * scale + size.width / 2;
-      final double y = size.height - 1 - point.y * scale - size.height / 2;
-      canvas.drawRect(Rect.fromLTWH(x, y, 1, 1), paint);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
   }
 }
